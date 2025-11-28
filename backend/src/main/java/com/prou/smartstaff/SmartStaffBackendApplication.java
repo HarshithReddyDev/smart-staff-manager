8c887ac8bb7c784d2736feb3eb78c8f476b76dbc
Harshith Reddy <your_email@example.com>
2025-11-28 10:17:58 +0530

    refactor: Move Spring Boot project into backend folder

---
+++ backend/src/main/java/com/prou/smartstaff/SmartStaffBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prou.smartstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStaffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStaffBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/prou/smartstaff/SmartStaffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prou.smartstaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStaffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
